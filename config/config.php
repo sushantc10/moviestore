b7a2690679108351cd5bb2426a47184ef05ad68e
sushantc10 <haldonkar.sushant@gmail.com>
2019-01-09 00:38:06 +0530

    2

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "movierepository";
+$servername = "ec2-107-20-211-10.compute-1.amazonaws.com";
+$username = "nigmqslwjkfvbo";
+$password = "8e171e1dfeb873a4deac17ab50804bbe4205df90b10facf90ca4e5557c49cee3";
+$database = "d7sbvomc9bre6q";
 
 $conn = new mysqli($servername,$username,$password,$database);
 
